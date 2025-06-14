ac18ec8f5b76cca3b8f6bc5a2e4ce8fb0a088e60
André Kanakis <andrekanakis@gmail.com>
2025-06-14 22:09:17 +0200

    feat: ✨added plugins

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -3,28 +3,27 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	-- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{ "projekt0n/github-nvim-theme", name = "github-dark-dimmed" },
+  { 'projekt0n/github-nvim-theme', name = 'github-dark-dimmed' },
 
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {},
 		-- stylua: ignore
 		keys = {
 			{ "gw", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 		},
-	},
+  },
 
-	{
-		"s1n7ax/nvim-window-picker",
-		name = "window-picker",
-		event = "VeryLazy",
-		version = "2.*",
-		config = function()
-			require("window-picker").setup()
-		end,
-	},
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup()
+    end,
+  },
 }

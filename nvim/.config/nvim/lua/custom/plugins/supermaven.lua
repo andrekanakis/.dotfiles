a849a357d7b95b0b3c5cb0cc54a39f3e95b5498b
André Kanakis <andrekanakis@gmail.com>
2025-06-15 21:22:50 +0200

    chore: 🔧 update supermaven.lua, let nvim.cmp handle tab completion

--- nvim/.config/nvim/lua/custom/plugins/supermaven.lua
+++ nvim/.config/nvim/lua/custom/plugins/supermaven.lua
@@ -2,11 +2,12 @@ return {
   'supermaven-inc/supermaven-nvim',
   opts = {
     keymaps = {
-      accept_suggestion = '<Tab>',
+      -- accept_suggestion = '<Tab>',
+      accept_suggestion = nil, -- use nvim-cmp for completion
       clear_suggestion = '<C-]>',
       accept_word = '<C-j>',
     },
-    disable_filetypes = { 'text', 'markdown' },
+    disable_filetypes = { 'text', 'markdown', 'csv', 'md', '.md' },
 
     log_level = 'warn',
   },
